d1578caec8171b6c1c04476bcdfdb5da676aefb6
pabloegan <paulhenryegan@gmail.com>
2016-09-21 13:10:53 +0100

    SuperClass files added

---
+++ SuperClass.java
@@ -0,0 +1,20 @@
+public class SuperClass {
+    
+    public SuperClass(){
+        
+        
+    }
+    
+    
+    int c;
+    int d;
+    
+    
+    public SuperClass (int xx, int yy){
+        
+        c = xx;
+        d = yy;
+    }
+    
+    
+}
